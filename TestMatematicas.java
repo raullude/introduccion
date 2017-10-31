8da5462ddfe6513c4d1efbb51926f920a2ec5d47
Raul Luque <raul.luque.delgado@gmail.com>
2017-10-31 11:56:27 +0100

    En la rama desarrollo 	modificado:    TestMatematicas.java

--- TestMatematicas.java
+++ TestMatematicas.java
@@ -7,7 +7,11 @@ public class TestMatematicas {
 		boolean resultadoUno = Matematicas.esPar(numero);
 		System.out.println("El nº " + numero + " ¿Es par? " + resultadoUno); 
 		
+		boolean resultadoDos = Matematicas.esDivisiblePorTres(numero);
+		System.out.println("El nº " + numero + " ¿es divisible por tres? " + resultadoDos);
 		
+		boolean resultadoTres = Matematicas.esDivisiblePorCinco(numero);
+		System.out.println("El nº " + numero + " ¿es divisible por cinco? " + resultadoTres);
 		
 	}
 }
