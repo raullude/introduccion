41d52f562e53a1fb2b0c1c4f8b26f6b49b2a7f6d
Raul Luque <raul.luque.delgado@gmail.com>
2017-10-31 11:54:57 +0100

    En la rama master 	nuevo archivo: Matematicas.java 	nuevo archivo: TestMatematicas.java

---
+++ Matematicas.java
@@ -0,0 +1,16 @@
+public class Matematicas {
+		
+	public static boolean esPar (int numero){	
+	return numero % 2 == 0;
+	}
+	
+	public static boolean esDivisiblePorTres (int numero){	
+	return numero % 3 == 0;
+	}
+	
+	public static boolean esDivisiblePorCinco (int numero){	
+	return numero % 5 == 0;
+	}
+	
+}
+
---
+++ TestMatematicas.java
@@ -0,0 +1,14 @@
+public class TestMatematicas {
+	
+	public static void main (String args[]) {
+		
+		int numero = 18;
+		
+		boolean resultadoUno = Matematicas.esPar(numero);
+		System.out.println("El nº " + numero + " ¿Es par? " + resultadoUno); 
+		
+		
+		
+	}
+}
+
